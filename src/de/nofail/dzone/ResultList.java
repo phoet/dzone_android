ec539868c949610aa4d608f805a0351f8e85b90e
phoet <phoetmail@googlemail.com>
2011-01-27 13:29:36 +0100

    load list asynchronous

--- src/de/nofail/dzone/ResultList.java
+++ src/de/nofail/dzone/ResultList.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,25 +18,35 @@ public class ResultList extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// initialize
-		final List<Item> items = NetHelper.getItems();
+		loadItems();
+	}
 
-		// prepare view
-		String[] titles = new String[items.size()];
-		for (int i = 0; i < titles.length; i++) {
-			titles[i] = items.get(i).title;
-		}
-		ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
-		setListAdapter(adapter);
-
-		// prepare actions
-		getListView().setOnItemClickListener(new OnItemClickListener() {
+	private void loadItems() {
+		new AsyncTask<Void, Void, List<Item>>() {
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Intent intent = new Intent(getApplicationContext(), Details.class);
-				intent.putExtra(Details.EXTRA_ITEM, items.get(position));
-				startActivity(intent);
+			protected List<Item> doInBackground(Void... params) {
+				return NetHelper.getItems();
 			}
-		});
+
+			@Override
+			protected void onPostExecute(final List<Item> items) {
+				String[] titles = new String[items.size()];
+				for (int i = 0; i < titles.length; i++) {
+					titles[i] = items.get(i).title;
+				}
+				ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, titles);
+				setListAdapter(adapter);
+
+				// prepare actions
+				getListView().setOnItemClickListener(new OnItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+						Intent intent = new Intent(getApplicationContext(), Details.class);
+						intent.putExtra(Details.EXTRA_ITEM, items.get(position));
+						startActivity(intent);
+					}
+				});
+			};
+		}.execute();
 	}
 }
