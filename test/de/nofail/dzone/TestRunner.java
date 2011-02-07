5c4aa0b1586a3336c33104928ce51bcceb253838
phoet <phoetmail@googlemail.com>
2011-02-07 21:49:57 +0100

    add unit tests

---
+++ test/de/nofail/dzone/TestRunner.java
@@ -0,0 +1,20 @@
+package de.nofail.dzone;
+
+import junit.framework.TestSuite;
+import android.test.InstrumentationTestRunner;
+import android.test.InstrumentationTestSuite;
+
+public class TestRunner extends InstrumentationTestRunner {
+
+	@Override
+	public TestSuite getAllTests() {
+		TestSuite suite = new InstrumentationTestSuite(this);
+		suite.addTestSuite(ItemTest.class);
+		return suite;
+	}
+
+	@Override
+	public ClassLoader getLoader() {
+		return TestRunner.class.getClassLoader();
+	}
+}
